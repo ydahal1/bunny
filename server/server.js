95eb2760b37ba98f99ad78da1176c8fa337f0e20
yadhap dahal <yadhapdahal@yadhaps-MBP.attlocal.net>
2022-06-01 00:12:35 -0400

    added env

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 const port = 3000;
 
@@ -7,9 +8,10 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-const logMessage = () => setInterval(() =>{
-  console.log(`App running at port ${port} ------${process.env.name}`);
-}, 5000)
+const logMessage = () =>
+  setInterval(() => {
+    console.log(`App running at port ${port} ------${process.env.name}`);
+  }, 5000);
 app.listen(3000, () => {
-   logMessage();
+  logMessage();
 });
